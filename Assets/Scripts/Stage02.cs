881a3f5d8ad91a711f7a59516307d1e06ec648a7
ouzhigang <ouzhiganggba@gmail.com>
2021-01-23 16:01:59 +0800

    调整第2关之中

--- Assets/Scripts/Stage02.cs
+++ Assets/Scripts/Stage02.cs
@@ -10,9 +10,20 @@ public class Stage02 : MonoBehaviour
     {
         EnemyData = new Dictionary<float, string[]>();
 
-        EnemyData.Add(8f, new string[] { "IsBoss:1" });
-        //EnemyData.Add(10f, new string[] { "Name:Enemy22,InitX:0" });
-        EnemyData.Add(10f, new string[] { "Name:Enemy02,InitX:1" });
+        EnemyData.Add(2f, new string[]
+        {
+            "Name:Enemy02,InitX:0",
+            "Name:Enemy02,InitX:1",
+            "Name:Enemy02,InitX:2",
+        });
+        EnemyData.Add(2.2f, new string[]
+        {
+            "Name:Enemy08,InitX:3,MoveTargetY:5",
+            "Name:Enemy08,InitX:-2.5,MoveTargetY:4.5",
+            "Name:Enemy08,InitX:1,MoveTargetY:4.5",
+        });
+        EnemyData.Add(24f, new string[] { "IsBoss:1" });
+        EnemyData.Add(26f, new string[] { "Name:Enemy22,InitX:0" });
     }
 
     // Start is called before the first frame update
